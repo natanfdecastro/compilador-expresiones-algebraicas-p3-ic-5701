2aaece481caddf603e697c1a4eeedbef69bac1b7
Kevin <kevin.rojas277@outlook.es>
2020-08-08 20:28:30 -0600

    [*] logica para creacion de la tabla y analisis semantico. [+] metodo crear reporte semantico txt

--- src/main/python/edu/tec/ic5710/SemanticAnalyser.py
+++ src/main/python/edu/tec/ic5710/SemanticAnalyser.py
@@ -1,10 +1,11 @@
 from src.main.python.edu.tec.ic5710.AbstractSyntaxTree import *
 from src.main.python.edu.tec.ic5710.SyntaxAnalyser import *
+from src.main.python.edu.tec.ic5710.configuration import NEW_PATH
 
 
 class SemanticAnalyser:
 
-    tables = [dict()]
+    tables = dict()
     tables_length = 1
     abstract_syntax_tree = []
 
@@ -21,15 +22,7 @@ class SemanticAnalyser:
         (i de la variable en la tabla, info de la variable)
         """
 
-        search = 0
-        n = 0
-        for table in self.tables:
-            search = table.get(name)
-
-            if type(search) == list:
-                return n, table[name]
-            n += 1
-        return -1, search
+        return self.tables.get(name)
 
     def visit_program_root_node(self, node):
 
@@ -43,29 +36,74 @@ class SemanticAnalyser:
                 break
         return state
 
-    def visit_declaration_node(self, node):
+    def visit_factor_node(self, node):
 
-        # insertar el nombre con el tipo y valor del tipo de la declaracion
-        node_type = node.children_nodes[0].visit(self)
-        node_value = node.children_nodes[1].children_nodes[0].node_value
-        table, search = self.check_table(node_value)
-        if table == -1:  # no esta en la tabla
-            self.tables[0][node_value] = node_type
-            return node.children_nodes[1].visit(self)
+        for child in node.children_nodes:
 
-        else:
-            return "ERROR: The variable " + node_value + ", " + node_type + " has been declared before"
+
+            if(child.node_type == "EXPRESSION"):
+                child.visit(self)
+
+            if(child.node_type == "IDENTIFIER"):
+                node_value = child.node_value
+                node_type = child.node_value
+
+                dict_value = self.check_table(node_value)
+
+                if dict_value:
+                    info = node.children_nodes[0]
+                else:
+                    raise Exception("ERROR: The variable " + node_value + ", " + node_type + " has not been assigned")
+
+
+    def visit_expression_node(self, node):
+        node.children_nodes[0].visit(self)
+
 
     def visit_assignation_node(self, node):
-
         # Buscar que el nombre exista
         node_type = node.children_nodes[0].node_type
         node_value = node.children_nodes[0].node_value
-        table, search = self.check_table(node_type)
 
-        if table != -1:
+        dict_value = self.check_table(node_value)
 
-            info = node.children_nodes[1].visit(self)
+        node.children_nodes[1].visit(self)
+        self.tables.update({node_value : node_type})
 
-        return "ERROR: The variable " + node_value + ", " + node_type + " has not been assigned"
+
+    def visit_print_production_node(self, node):
+
+        node_value = node.children_nodes[1].node_value
+        node_type = node.children_nodes[1].node_type
+
+        dict_value = self.check_table(node_value)
+
+        if dict_value:
+            info = node.children_nodes[1]
+        else:
+            raise Exception("ERROR: The variable " + node_value + ", " + node_type + " has not been assigned")
+
+
+
+    def create_lexical_analysis_report(self):
+        print("\nETAPA ANALISIS SEMANTICO: COMPLETADA CORRECTAMENTE")
+        try:
+            file = open(NEW_PATH + '/semantic_analysis_report.txt', 'w')
+            file.close()
+
+            file = open(NEW_PATH + '/semantic_analysis_report.txt', 'a')
+            file.write("SEMANTIC TABLE: \n\n")
+            count = 0
+            for key in self.tables:
+                file.write(str(count) + " { KEY: " + str(key) + ", VALUE: " + self.tables[key] + " }\n")
+                count += 1
+
+            file.close()
+
+            print(">>> NUEVO REPORTE DE ANALISIS SEMANTICO CREADO")
+
+        except Exception as e:
+            print(str(e), ">>> ERROR GENERANDO REPORTE DE SEMANTICO LEXICO")
+
+
 
