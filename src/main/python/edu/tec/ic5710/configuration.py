d3559cdfa232d70630c8495141c77da2f114f9de
Kevin <kevin.rojas277@outlook.es>
2020-08-10 21:31:15 -0600

    [*] absolute path del configuration para ejecutar por comando.

--- src/main/python/edu/tec/ic5710/configuration.py
+++ src/main/python/edu/tec/ic5710/configuration.py
@@ -4,16 +4,16 @@ import os
 
 # Valida el sistema operativo para adaptar el path de busqueda
 if os.name == 'posix':
-    path = os.getcwd().split("/")
+    path = os.path.abspath(__file__).split("/")
 else:
-    path = os.getcwd().split("\\")
+    path = os.path.abspath(__file__).split("\\")
 
 
 # Este va ser el nuevo path para buscar las pruebas
 NEW_PATH = ""
 
 # Insertar todos los paths menos el último, exceptuando la carpeta main
-for i in range(len(path) - 4):
+for i in range(len(path) - 5):
     NEW_PATH += path[i] + '/'
 
 
